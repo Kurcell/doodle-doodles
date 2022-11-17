8bc1de6e312d4eabf7ec2296fadd1c3e21ab461b
kpurcell <kpurcell@optivon.net>
2022-11-17 18:47:16 -0500

    added default route

--- src/routes/doodles.py
+++ src/routes/doodles.py
@@ -3,6 +3,10 @@ from ..models.models import Doodle
 
 doodle_bp = Blueprint('doodles', __name__)
 
+@doodle_bp.route("/", methods=['GET'])
+def read():
+    return "Welcome to Just a Doodle's Doodle API!"
+
 @doodle_bp.route("/doodles", methods=['GET'])
 def read():
     return jsonify(Doodle.query.all())
