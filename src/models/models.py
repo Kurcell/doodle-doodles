e18c8691cc5833da0c74741f625c9a5b04e93bcf
kpurcell <kpurcell@optivon.net>
2022-11-17 19:16:16 -0500

    Create Doodle model and routes

--- src/models/models.py
+++ src/models/models.py
@@ -1,18 +1,47 @@
+import string
 from .. import db
 from dataclasses import dataclass
 
 @dataclass 
 class Doodle(db.Model):
     __tablename__ = "doodle"
-    
+
+    id: int
+    instructions: string
+
     id = db.Column(db.Integer, primary_key=True)
-    instructions = db.Column(db.String(500), nullable=False)
+    instructions = db.Column(db.String(), nullable=False)
+
+    def __init__(self, id, instructions):
+        self.id = id
+        self.instructions = instructions
 
     @staticmethod
     def create(instructions):
         """
         Create new doodle
         """
-        doodle = Doodle(instructions)
+        doodle = Doodle(None, instructions)
         db.session.add(doodle)
         db.session.commit()
+        return doodle
+
+    @staticmethod
+    def update(id, instructions):
+        """
+        Update existing doodle
+        """
+        doodle = Doodle.query.filter_by(id = id).one()
+        doodle.instructions = instructions
+        db.session.commit()
+        return doodle
+    
+    @staticmethod
+    def delete(id):
+        """
+        Delete existing doodle
+        """
+        doodle = Doodle.query.filter_by(id = id).one()
+        db.session.delete(doodle)
+        db.session.commit()
+        return doodle
--- src/routes/doodles.py
+++ src/routes/doodles.py
@@ -1,12 +1,31 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 from ..models.models import Doodle
+from ..__init__ import db
 
 doodle_bp = Blueprint('doodles', __name__)
 
 @doodle_bp.route("/", methods=['GET'])
-def read():
+def hello():
     return "Welcome to Just a Doodle's Doodle API!"
 
+@doodle_bp.route("/doodle/<int:id>", methods=['GET'])
+def readOne(id):
+    return jsonify(Doodle.query.filter_by(id = id).one())
+
 @doodle_bp.route("/doodles", methods=['GET'])
-def read():
+def readMany():
     return jsonify(Doodle.query.all())
+
+@doodle_bp.route("/doodle", methods=['POST'])
+def create():
+    req = request.get_json()
+    return jsonify(Doodle.create(req['instructions']))
+
+@doodle_bp.route("/doodle/<int:id>", methods=['PUT'])
+def update(id):
+    req = request.get_json()
+    return jsonify(Doodle.update(id, req['instructions']))
+
+@doodle_bp.route("/doodle/<int:id>", methods=['DELETE'])
+def delete(id):
+    return jsonify(Doodle.delete(id))
